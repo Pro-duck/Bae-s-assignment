51b4a58c4ee4d961069dc3f7f18f68207a5d9496
Producktivity021 <88133273+Pro-duck@users.noreply.github.com>
2021-09-18 17:58:11 +0900

    Delete plusOneCounter.js

--- plusOneCounter.js
+++
@@ -1,8 +0,0 @@
-var plusOne = 0;
-            function addonefunc() {
-                plusOne = plusOne+ 1;
-                document.getElementById("plusOne").innerHTML = plusOne;};
-       
-            function resetfunc() {
-                plusOne = 0;
-            document.getElementById("plusOne").innerHTML = plusOne;};
