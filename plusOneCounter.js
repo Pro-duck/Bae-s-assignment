e1c95fed4c756cd99301957748081027628255df
Producktivity021 <88133273+Pro-duck@users.noreply.github.com>
2021-09-18 17:58:35 +0900

    Add files via upload

---
+++ plusOneCounter.js
@@ -0,0 +1,8 @@
+var plusOne = 0;
+            function addonefunc() {
+                plusOne = plusOne+ 1;
+                document.getElementById("plusOne").innerHTML = plusOne;};
+       
+            function resetfunc() {
+                plusOne = 0;
+            document.getElementById("plusOne").innerHTML = plusOne;};
