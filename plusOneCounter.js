5a9daf7c1a6dcc06daa758f70ddb4d746dd40c1d
Producktivity021 <88133273+Pro-duck@users.noreply.github.com>
2021-09-18 20:20:11 +0900

    Update plusOneCounter.js

--- plusOneCounter.js
+++ plusOneCounter.js
@@ -1,10 +1,13 @@
-var plusOne = 0;
-            function addonefunc() {
-                plusOne = plusOne+ 1;
-                document.getElementById("plusOne").innerHTML = plusOne;}; 
-// addonefunc() 함수를 정의. innerHTML 속성을 통해 HTML에 접근 후, plusOne과 일치하는 id 속성에 접근하여  plusOne = plusOne+ 1이 수행된 값을 돌려줌.
+var plusOne = 0; //plusOne이라는 변수를 선언하고 0으로 초기화.
+            function addonefunc() { // 함수 addonefunc()를 선언.
+                plusOne = plusOne+ 1; //plusOne에 plusOne에 1이 더해진 값을 대입.
+                document.getElementById("plusOne").innerHTML = plusOne;}; /*해당 파일을 호출한 HTML 문서 내부에서 매개변수 plusOne과 같은 ID를 가진 태그를 호출 후, 
+                해당 태그 안에 쓰여지는 HTML 형식의 값을 변수값으로 넣어준 것.*/
+                                                                          
+
        
-            function resetfunc() {
-                plusOne = 0;
-            document.getElementById("plusOne").innerHTML = plusOne;};
-// resetfunc() 함수를 정의. innerHTML 속성을 통해 HTML에 접근 후, plusOne과 일치하는 id 속성에 접근하여  plusOne = 0이 수행된 값을 돌려줌. 
+            function resetfunc() { // 함수 resetfunc()를 선언.
+                plusOne = 0; //plusOne에 0을 대입.
+            document.getElementById("plusOne").innerHTML = plusOne;}; /*해당 파일을 호출한 HTML 문서 내부에서 매개변수 plusOne과 같은 ID를 가진 태그를 호출 후, 
+                해당 태그 안에 쓰여지는 HTML 형식의 값을 변수값으로 넣어준 것.*/
+
